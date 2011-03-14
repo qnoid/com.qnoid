64208f1cdcb3aa9d5f2ce822be73cefac49ef0d8
Markos Charatzas <markos.charatzas@gmail.com>
2011-03-14 16:46:25 +0000

    (+) added source for http://forrst.com/posts/How_to_think_of_Macros-trj

---
+++ src/main/java/com/forrst/java/ttrj/RobotMethods.java
@@ -0,0 +1,130 @@
+/*
+ *  This file is under the license Attribution-ShareAlike 3.0 Unported 
+ *  (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ *  You are free:
+ *      to Share - to copy, distribute and transmit the work
+ *      to Remix - to adapt the work
+ *
+ *  Under the following conditions:
+ *
+ *  Attribution - You must attribute the work in the manner specified by the 
+ *  author or licensor (but not in any way that suggests that they endorse you 
+ *  or your use of the work).
+ *  
+ *  Share Alike - If you alter, transform, or build upon this work, you may 
+ *  distribute the resulting work only under the same or similar license to 
+ *  this one.
+ *
+ *  With the understanding that:
+ *
+ *  Waiver - Any of the above conditions can be waived if you get permission 
+ *  from the copyright holder.
+ *
+ *  Public Domain - Where the work or any of its elements is in the public 
+ *  domain under applicable law, that status is in no way affected by the 
+ *  license.
+ *
+ *  Other Rights - In no way are any of the following rights affected by the 
+ *  license:
+ *  Your fair dealing or fair use rights, or other applicable copyright 
+ *  exceptions and limitations;
+ *
+ *  The author's moral rights;
+ *  Rights other persons may have either in the work itself or in how the work 
+ *  is used, such as publicity or privacy rights.
+ *
+ */
+package com.forrst.java.ttrj;
+
+/**
+ * All available {@link RobotMethod}s
+ * 
+ * @author Markos Charatzas [http://forrst.me/Cue]
+ * @date Mar 14, 2011
+ */
+public class RobotMethods
+{
+    /**
+     * Encapsulates the {@link Robot#log(String)}
+     */
+    private static final class MessageMethod implements RobotMethod
+    {
+        private final String message;
+
+        /**
+         * @param message
+         */
+        private MessageMethod(String message)
+        {
+            this.message = message;
+        }
+
+        @Override
+        public void execute(Robot robot) {
+            robot.log(this.message);
+        }
+    }
+
+    /**
+     * Encapsulates the {@link Robot#alert()} method
+     */
+    private static final class AlertMethod implements RobotMethod
+    {
+        @Override
+        public void execute(Robot robot) {
+            robot.alert();
+        }
+    }
+
+    /**
+     * Encapsulates the {@link Robot#move(int, int)} method
+     */
+    private static final class MoveMethod implements RobotMethod
+    {
+        private final int y;
+        private final int x;
+
+        /**
+         * @param y
+         * @param x
+         */
+        private MoveMethod(int y, int x)
+        {
+            this.y = y;
+            this.x = x;
+        }
+
+        @Override
+        public void execute(Robot robot) {
+            robot.move(this.x, this.y);
+        }
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @return a {@link RobotMethod} that makes a robot move to the specified coordinates
+     * @see Robot#move(int, int)
+     */
+    public static RobotMethod newMove(int x, int y) {
+    return new MoveMethod(y, x);
+    }
+
+    /**
+     * @return a {@link RobotMethod} that makes a robot alert
+     * @see Robot#alert()
+     */
+    public static RobotMethod newAlert() {
+    return new AlertMethod();
+    }
+
+    /**
+     * @param message the message to display
+     * @return a {@link RobotMethod} that makes a robot display the specified message
+     * @see Robot#log(String)
+     */
+    public static RobotMethod newMessage(String message) {
+    return new MessageMethod(message);
+    }
+}
---
+++ src/main/java/com/forrst/java/ttrj/RobotTracker.java
@@ -0,0 +1,73 @@
+/*
+ *  This file is under the license Attribution-ShareAlike 3.0 Unported 
+ *  (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ *  You are free:
+ *      to Share - to copy, distribute and transmit the work
+ *      to Remix - to adapt the work
+ *
+ *  Under the following conditions:
+ *
+ *  Attribution - You must attribute the work in the manner specified by the 
+ *  author or licensor (but not in any way that suggests that they endorse you 
+ *  or your use of the work).
+ *  
+ *  Share Alike - If you alter, transform, or build upon this work, you may 
+ *  distribute the resulting work only under the same or similar license to 
+ *  this one.
+ *
+ *  With the understanding that:
+ *
+ *  Waiver - Any of the above conditions can be waived if you get permission 
+ *  from the copyright holder.
+ *
+ *  Public Domain - Where the work or any of its elements is in the public 
+ *  domain under applicable law, that status is in no way affected by the 
+ *  license.
+ *
+ *  Other Rights - In no way are any of the following rights affected by the 
+ *  license:
+ *  Your fair dealing or fair use rights, or other applicable copyright 
+ *  exceptions and limitations;
+ *
+ *  The author's moral rights;
+ *  Rights other persons may have either in the work itself or in how the work 
+ *  is used, such as publicity or privacy rights.
+ *
+ */
+package com.forrst.java.ttrj;
+
+
+/**
+ * Keeps track of the every {@link Robot} created using 
+ * {@link RobotFactory#newRobot()} 
+ * 
+ * @author Markos Charatzas [http://forrst.me/Cue]
+ * @date Mar 14, 2011
+ */
+final class RobotTracker
+{
+    private Bug bug;
+
+    /**
+     * Called when a new robot is created.
+     * 
+     * Holds only the bug to the last created robot
+     * 
+     * @param robot the robot that was created by {@link RobotFactory#newRobot()}
+     * @return a bug tracking all methods executed on the specified robot
+     */
+    public Bug wasCreated(Robot robot) {
+        
+        this.bug = new Bug(robot);
+        
+    return bug;
+    }
+
+    /**
+     * @return the bug for the last robot created 
+     */
+    public Bug latestBug(){
+    return this.bug;
+    }
+}
